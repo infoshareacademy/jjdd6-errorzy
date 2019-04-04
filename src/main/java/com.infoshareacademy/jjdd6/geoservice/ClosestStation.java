5cecedb0d05ef0aee98b156e905ce3e00b3d73b4
Pawel Krzesinsk <krzesinski.pawel@gmail.com>
2019-04-04 15:33:29 +0200

    New input parameter in getDistanceBetween... method.

--- src/main/java/com.infoshareacademy/jjdd6/geoservice/ClosestStation.java
+++ src/main/java/com.infoshareacademy/jjdd6/geoservice/ClosestStation.java
@@ -1,14 +1,17 @@
 package com.infoshareacademy.jjdd6.geoservice;
 
+import com.infoshareacademy.jjdd6.errorzy.Place;
+import com.infoshareacademy.jjdd6.userinput.GetUserInput;
+
 public class ClosestStation {
     public final static double EARTH_RADIUS_IN_METERS = 6371*1000;
 
-    public static double getDistanceBetweenTwoGeoPoints(double lat1, double lat2, double lon1, double lon2) {
+    public static double getDistanceBetweenTwoGeoPoints(double lat1, double lat2, Place place) {
 
-
+        GetUserInput.getDoubleFromUser("Please insert geographic coordinate system (Longitude): ");
 
         double latDistance = Math.toRadians(lat2 - lat1);
-        double lonDistance = Math.toRadians(lon2 - lon1);
+        double lonDistance = Math.toRadians(place.);
         double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
                 + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
                 * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
