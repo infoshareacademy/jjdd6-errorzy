70daebf902d95d2d0ea684f6619cf6d3463f2b70
Pawel Krzesinsk <krzesinski.pawel@gmail.com>
2019-04-05 16:53:02 +0200

    findStationsWithinRadius and getNumberOfStationsWithinRadius methods implemented.

--- src/main/java/com.infoshareacademy/jjdd6/geoservice/StationsInMyArea.java
+++ src/main/java/com.infoshareacademy/jjdd6/geoservice/StationsInMyArea.java
@@ -2,20 +2,27 @@ package com.infoshareacademy.jjdd6.geoservice;
 
 import com.infoshareacademy.jjdd6.errorzy.Place;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StationsInMyArea {
-    public List<Place> findStationsInArea(double lat, double lng, double radius) {
+
+    public List<Place> findStationsWithinRadius(double lat, double lng, double radius) {
         //TODO insert correct placeList.
         List<Place> placeList = ClosestStation.mockedPlaceList();
         ClosestStation closestStation = new ClosestStation();
+        List<Place> placesWithinRadius = new ArrayList<>();
 
         for (Place place : placeList) {
-
-            closestStation.getDistanceBetweenTwoGeoPoints();
+            double distanceToStation = closestStation.getDistanceBetweenTwoGeoPoints(lat, lng, place);
+            if (distanceToStation <= radius) {
+                placesWithinRadius.add(place);
+            }
         }
+        return placesWithinRadius;
+    }
 
-
-        return null;
+    public int getNumberOfStationsWithinRadius(List<Place> placeList) {
+        return placeList.size();
     }
 }
