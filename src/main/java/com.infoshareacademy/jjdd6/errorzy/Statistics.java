facb9116b31c700d01806bcc21ffe9a86d7c32d7
piokowalski <piokowalski@onet.pl>
2019-04-09 14:28:24 +0200

    Statistics class merged into project pulled from develop

---
+++ src/main/java/com.infoshareacademy/jjdd6/errorzy/Statistics.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy.jjdd6.errorzy;
+
+
+public class Statistics {
+
+    public void countryStats(Integer bikeStations, String country) {
+        System.out.println("There is: " + bikeStations + " bike stations in "+country);
+    }
+
+    public void cityStats(Integer bikeStations, String city) {
+        System.out.println("There is: " + bikeStations + " bike stations in "+city);
+    }
+}
