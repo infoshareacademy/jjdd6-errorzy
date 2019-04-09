f738f9531b565c2f0424cb32d308bddcd19ca790
piokowalski <piokowalski@onet.pl>
2019-04-09 15:22:43 +0200

    Logger class created, w/ Log4j2 (Log.java)

---
+++ src/main/java/com.infoshareacademy/jjdd6/errorzy/Log.java
@@ -0,0 +1,11 @@
+package com.infoshareacademy.jjdd6.errorzy;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
+
+
+public class Log {
+    private static final Logger logger = LogManager.getLogger(this.getClass().getName());
+}
--- src/main/java/com.infoshareacademy/jjdd6/errorzy/Statistics.java
+++ src/main/java/com.infoshareacademy/jjdd6/errorzy/Statistics.java
@@ -1,7 +1,17 @@
 package com.infoshareacademy.jjdd6.errorzy;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Statistics {
+
+    Logger logger = LogManager.getRootLogger();
+
+
+
+
+
 
     public static void countryStats(Integer bikeStations, String country) {
         System.out.println("There is: " + bikeStations + " bike stations in "+country);
