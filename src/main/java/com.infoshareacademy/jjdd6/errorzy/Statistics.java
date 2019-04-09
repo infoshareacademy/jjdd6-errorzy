4aca22fb55762dffba61175e5a777785a628cf51
piokowalski <piokowalski@onet.pl>
2019-04-09 14:41:00 +0200

    Statistics class inside the project

--- src/main/java/com.infoshareacademy/jjdd6/errorzy/Statistics.java
+++ src/main/java/com.infoshareacademy/jjdd6/errorzy/Statistics.java
@@ -3,12 +3,14 @@ package com.infoshareacademy.jjdd6.errorzy;
 
 public class Statistics {
 
-    public void countryStats(Integer bikeStations, String country) {
+    public static void countryStats(Integer bikeStations, String country) {
         System.out.println("There is: " + bikeStations + " bike stations in "+country);
     }
 
-    public void cityStats(Integer bikeStations, String city) {
+    public static void cityStats(Integer bikeStations, String city) {
         System.out.println("There is: " + bikeStations + " bike stations in "+city);
 
     }
+
+
 }
