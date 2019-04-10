93a1b40a084046aa7c1b562944623cbd39483b8a
Pawel Krzesinsk <krzesinski.pawel@gmail.com>
2019-04-10 14:48:36 +0200

    Type corrected and printBikeLogo and printMenuText methods added.

--- src/main/java/com.infoshareacademy/jjdd6/menu/Menu.java
+++ src/main/java/com.infoshareacademy/jjdd6/menu/Menu.java
@@ -6,7 +6,32 @@ import java.util.Scanner;
 public class Menu {
 
 
-    public static int startAplicationMenu() {
+    public static int startApplicationMenu() {
+        printBikeLogo();
+
+        printMenuText();
+
+        Scanner scanner = new Scanner(System.in);
+        return Integer.parseInt(scanner.nextLine());
+    }
+
+    private static void printMenuText() {
+        System.out.println();
+        System.out.println("     ****************************************");
+        System.out.println("     *                 MENU                 *");
+        System.out.println("     ****************************************");
+        System.out.println("     1. Find free Bike");
+        System.out.println("     2. Find the nearest place");
+        System.out.println("     3. Show list of Countries");
+        System.out.println("     4. Show list of Cities");
+        System.out.println("     5. Show statistics for Countries or Cities");
+        System.out.println();
+        System.out.println("     To start available options select number ");
+        System.out.println("     To exit press 0 ");
+        System.out.println("     Select number ");
+    }
+
+    private static void printBikeLogo() {
         System.out.println();
         System.out.println();
         System.out.println("                                          $\"   *.      \n" +
@@ -27,23 +52,6 @@ public class Menu {
                 "    %.         z*\"                      ^%.        .r\"\n" +
                 "       \"*==*\"\"                             ^\"*==*\"\"   ");
         System.out.println();
-        System.out.println();
-        System.out.println("     ****************************************");
-        System.out.println("     *                 MENU                 *");
-        System.out.println("     ****************************************");
-        System.out.println("     1. Find free Bike");
-        System.out.println("     2. Find the nearest place");
-        System.out.println("     3. Show list of Countries");
-        System.out.println("     4. Show list of Cities");
-        System.out.println("     5. Show statistics for Countries or Cities");
-        System.out.println();
-        System.out.println("     To start available options select number ");
-        System.out.println("     To exit press 0 ");
-        System.out.println("     Select number ");
-
-
-        Scanner scanner = new Scanner(System.in);
-        return Integer.parseInt(scanner.nextLine());
     }
 
     public static void main(String[] args) {
@@ -51,7 +59,7 @@ public class Menu {
         int select = -1;
         while (select != 0) {
             try {
-                select = startAplicationMenu();
+                select = startApplicationMenu();
             } catch (NumberFormatException e) {
                 System.err.println("This is not a fucking number!");
             }
