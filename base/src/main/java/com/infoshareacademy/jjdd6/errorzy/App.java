42ce3a76ef7ff73ebfaad834d0c9d6437907bb50
Pawel Krzesinsk <krzesinski.pawel@gmail.com>
2019-04-17 15:57:25 +0200

    Skeleton of multi-module project created.

---
+++ base/src/main/java/com/infoshareacademy/jjdd6/errorzy/App.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy.jjdd6.errorzy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
