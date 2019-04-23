b8344505b13c92ced1894ece797b6a2d850db531
Pawel Krzesinski <46893277+pkrzesinski@users.noreply.github.com>
2019-04-23 16:15:23 +0200

    JJDD6ERR-34 getCountryServlet (#23)
    * XmlUnmarshaller - main mathod deleted. Class XmlUnmarshaller injected into GetCountryServlet.
    
    * Some corrections made in Menu class.
    
    * Servlet with country list done.
    
    * GetCountryServlet class name changed to CountryServlet.
    
    * Path to nextbike-live.xml corrected.

---
+++ web/src/main/java/servlet/CountryServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import com.infoshareacademy.jjdd6.errorzy.Country;
+import com.infoshareacademy.jjdd6.errorzy.xmlunmarshaller.CountrySearch;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@WebServlet("/country-servlet")
+public class CountryServlet extends HttpServlet {
+
+    @Inject
+    private CountrySearch countrySearch;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int i = 1;
+        for (Map.Entry<String, Country> entry : countrySearch.getMapOfCountries().entrySet()) {
+            resp.getWriter().println(i + ". " + entry.getKey());
+            i++;
+        }
+    }
+}
