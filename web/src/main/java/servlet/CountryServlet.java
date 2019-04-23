23e29ab5a208dbb63d57e86eae40d5fd5e82a0eb
Pawel Krzesinsk <krzesinski.pawel@gmail.com>
2019-04-23 16:02:41 +0200

    GetCountryServlet class name changed to CountryServlet.

--- web/src/main/java/servlet/CountryServlet.java
+++ web/src/main/java/servlet/CountryServlet.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.Map;
 
 @WebServlet("/country-servlet")
-public class GetCountryServlet extends HttpServlet {
+public class CountryServlet extends HttpServlet {
 
     @Inject
     private CountrySearch countrySearch;
