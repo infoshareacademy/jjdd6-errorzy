43920679c69c41e1cff7b7446e652209218f752d
StepniakMaciej <48452281+StepniakMaciej@users.noreply.github.com>
2019-04-25 15:19:17 +0200

    Jjdd6 err 16 (#24)
    * upload pliku
    
    * upload servlet
    
    * upload pliku
    
    * poprawki
    
    * poprawki
    
    * poprawki

---
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/upload/FileUploadProcessorBean.java
@@ -0,0 +1,44 @@
+package com.infoshareacademy.jjdd6.errorzy.upload;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+@RequestScoped
+public class FileUploadProcessorBean {
+
+    private static final String SETTINGS_FILE = "settings.properties";
+
+    private static final Logger LOGGER = LogManager.getLogger(FileUploadProcessorBean.class.getName());
+
+    public File uploadFile(Part filePart) throws IOException {
+        String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString();
+
+
+        File file = new File(getUploadFilesPath() + fileName);
+
+       Files.deleteIfExists(file.toPath());
+
+        InputStream fileContent = filePart.getInputStream();
+
+        Files.copy(fileContent, file.toPath());
+
+        fileContent.close();
+
+        LOGGER.info("File has been saved  " + file.toPath() + " and size file is " + file.length());
+
+        return file;
+    }
+    public String getUploadFilesPath() throws IOException {
+        Properties settings = new Properties();
+        settings.load(Thread.currentThread()
+                .getContextClassLoader().getResource(SETTINGS_FILE).openStream());
+        return settings.getProperty("Upload.Path");
+    }
+}
