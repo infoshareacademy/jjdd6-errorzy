78249422af6a3f68fdf96f2b2f653c3faec0363e
piokowalski <piokowalski@onet.pl>
2019-05-03 15:57:08 +0200

    HTML sheets added, hello ftlh created (working)

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/CountryServlet.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/CountryServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
+
 @WebServlet("/countries")
 public class CountryServlet extends HttpServlet {
 
