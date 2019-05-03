a395083b6ba983966a24c92006af58f6b61413d1
piokowalski <piokowalski@onet.pl>
2019-05-03 15:55:01 +0200

    HTML sheets added, hello ftlh created (working)

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/CountryServlet.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/CountryServlet.java
@@ -1,7 +1,9 @@
 package com.infoshareacademy.jjdd6.errorzy.web;
 
 import com.infoshareacademy.jjdd6.errorzy.Country;
+import com.infoshareacademy.jjdd6.errorzy.freemarker.TemplateProvider;
 import com.infoshareacademy.jjdd6.errorzy.xmlunmarshaller.CountrySearch;
+import freemarker.template.Template;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -12,14 +14,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Map;
 
-@WebServlet("/country-servlet")
+@WebServlet("/countries")
 public class CountryServlet extends HttpServlet {
 
     @Inject
     private CountrySearch countrySearch;
+    TemplateProvider templateProvider;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Template template = templateProvider.getTemplate(getServletContext(), "countries.ftlh");
+        template.dump(resp.getWriter());
+
         int i = 1;
         for (Map.Entry<String, Country> entry : countrySearch.getMapOfCountries().entrySet()) {
             resp.getWriter().println(i + ". " + entry.getKey());
