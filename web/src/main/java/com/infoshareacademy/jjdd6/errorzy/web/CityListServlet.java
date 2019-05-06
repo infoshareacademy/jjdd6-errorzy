b2481fdb945b007f09d9ad9fe836a37daba4b458
Pawel Krzesinsk <krzesinski.pawel@gmail.com>
2019-05-06 15:45:11 +0200

    Some corrections of HTML done

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/CityListServlet.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/CityListServlet.java
@@ -28,6 +28,7 @@ public class CityListServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
         Writer writer = resp.getWriter();
         Template template = templateProvider.getTemplate(getServletContext(), "city-list-servlet.ftlh");
 
