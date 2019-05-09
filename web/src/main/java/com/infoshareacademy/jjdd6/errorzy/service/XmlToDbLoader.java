bc63988c5713f3155038453b70ca2ad3359efcb3
Pawel Krzesinsk <krzesinski.pawel@gmail.com>
2019-05-09 16:12:41 +0200

    XML loader to be finished.:

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/service/XmlToDbLoader.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/service/XmlToDbLoader.java
@@ -1,18 +1,31 @@
 package com.infoshareacademy.jjdd6.errorzy.service;
 
+import com.infoshareacademy.jjdd6.errorzy.Country;
 import com.infoshareacademy.jjdd6.errorzy.dao.CountryDao;
+import com.infoshareacademy.jjdd6.errorzy.xmlunmarshaller.CountrySearch;
 
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
-import javax.inject.Singleton;
+import java.util.List;
+import java.util.logging.Logger;
 
 @Singleton
 @Startup
 public class XmlToDbLoader {
 
+    Logger logger = Logger.getLogger(getClass().getName());
+
     @Inject
     private CountryDao countryDao;
+    @Inject
+    private CountrySearch countrySearch;
 
-
-
+    @Schedule(hour = "*", minute = "*/5", second = "*")
+    public void loadCountriesToDB() {
+        logger.info("hello from scheduler");
+        List<Country> countryList = countrySearch.getCountries();
+        countryList.forEach(country -> countryDao.save(country));
+    }
 }
