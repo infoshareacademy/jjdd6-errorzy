f8924d9078bd3f9bf08893fd42f06d9fab588a1b
Maciej Stepniak <maciej6294@gmail.com>
2019-05-09 16:16:43 +0200

    Statistics Servlet

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/StatisticsServlet.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/StatisticsServlet.java
@@ -1,8 +1,10 @@
 package com.infoshareacademy.jjdd6.errorzy.web;
 
+import com.infoshareacademy.jjdd6.errorzy.numberOfPlaces.Statistics;
 import com.infoshareacademy.jjdd6.errorzy.statistics.dao.CityStatisticsDao;
 import com.infoshareacademy.jjdd6.errorzy.statistics.dao.CountryStatisticsDao;
 import com.infoshareacademy.jjdd6.errorzy.statistics.dao.PlaceStatisticsDao;
+import freemarker.template.Template;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,6 +15,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet("/statistics")
 public class StatisticsServlet extends HttpServlet {
@@ -30,11 +35,52 @@ public class StatisticsServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
+        final String action = req.getParameter("action");
+        LOGGER.info("Requested action: {}", action);
+        if (action == null || action.isEmpty()) {
+            resp.getWriter().write("Empty action parameter.");
+            return;
+        }
+
+        if (action.equals("update")) {
+            updateStatiscics(req, resp);
+        } else if (action.equals("save")) {
+            saveStatiscics(req, resp);
+        } else if (action.equals("delete")) {
+            deleteStatiscics(req, resp);
+        } else if (action.equals("findByName")) {
+            findByNameStatiscics(req, resp);
+        } else if (action.equals("findMostChecked")) {
+            findMostCheckedStatiscics(req, resp);
+        } else if (action.equals("addToStatistics")) {
+            addToStatisticStatiscics(req, resp);
+        } else {
+            resp.getWriter().write("Unkown action.");
+        }
+
+
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    private void addToStatisticStatiscics(HttpServletRequest req, HttpServletResponse resp) {
+    }
+
+    private void findMostCheckedStatiscics(HttpServletRequest req, HttpServletResponse resp) {
+    }
+
+    private void findByNameStatiscics(HttpServletRequest req, HttpServletResponse resp) {
+    }
+
+
+    private void deleteStatiscics(HttpServletRequest req, HttpServletResponse resp) {
+    }
+
+    private void saveStatiscics(HttpServletRequest req, HttpServletResponse resp) {
+    }
+
+    private void updateStatiscics(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        final String name = new String(req.getParameter("name"));
+        LOGGER.info("Updating statistics with name = {}", name);
+        //final Statistics existingStatistics =;
     }
 }
