8389248a061f3061c7c6b4c90135d13eb93adb27
pkrzesinski <Krzesinski.pawel@gmail.com>
2019-05-14 00:09:42 +0200

    Revert "XML data loaded"
    This reverts commit 6cd64882fd0e793a3ecc3857ae1d8a36a610d88e.

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/service/CityXmlToDBLoader.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/service/CityXmlToDBLoader.java
@@ -5,16 +5,14 @@ import com.infoshareacademy.jjdd6.errorzy.dao.CityDao;
 import com.infoshareacademy.jjdd6.errorzy.model.CityModel;
 import com.infoshareacademy.jjdd6.errorzy.model.CountryModel;
 import com.infoshareacademy.jjdd6.errorzy.xmlunmarshaller.CitySearch;
-import com.infoshareacademy.jjdd6.errorzy.xmlunmarshaller.CountrySearch;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
+import java.util.logging.Logger;
 
 @Singleton
 public class CityXmlToDBLoader {
-    private static final Logger LOG = LogManager.getLogger(CountrySearch.class);
+    private static final Logger LOGGER = Logger.getLogger(CityXmlToDBLoader.class.getName());
 
     @EJB
     private CityDao cityDao;
@@ -35,5 +33,15 @@ public class CityXmlToDBLoader {
 
             placeXmlToDBLoader.loadPlaceModelToDataBase(city, cityModel);
         });
+
+//        citySearch.getMapOfCitiesForCountry(countryModel.getCountryName()).values().forEach(city -> {
+//            CityModel cityModel = new CityModel(city.getLat(),
+//                    city.getLng(),
+//                    city.getName(),
+//                    countryModel);
+//            cityDao.save(cityModel);
+//
+//            placeXmlToDBLoader.loadPlaceModelToDataBase(cityModel);
+//        });
     }
 }
--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/service/CountryXmlToDBLoader.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/service/CountryXmlToDBLoader.java
@@ -3,17 +3,16 @@ package com.infoshareacademy.jjdd6.errorzy.service;
 import com.infoshareacademy.jjdd6.errorzy.dao.CountryDao;
 import com.infoshareacademy.jjdd6.errorzy.model.CountryModel;
 import com.infoshareacademy.jjdd6.errorzy.xmlunmarshaller.CountrySearch;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.transaction.Transactional;
+import java.util.logging.Logger;
 
 @Singleton
 @Transactional
 public class CountryXmlToDBLoader {
-    private static final Logger LOG = LogManager.getLogger(CountrySearch.class);
+    private static final Logger LOGGER = Logger.getLogger(CountryXmlToDBLoader.class.getName());
 
     @EJB
     private CountryDao countryDao;
@@ -35,11 +34,11 @@ public class CountryXmlToDBLoader {
                     country.getCountryName());
 
             countryDao.save(countryModel);
-            LOG.info(countryModel.getCountryName() + ": Added to DB.");
+            LOGGER.info(countryModel.getCountryName() + ": Added to DB.");
 
             cityXmlToDBLoader.loadCityModelToDataBase(country, countryModel);
         });
 
-        LOG.info("Loading to DB finished.");
+        LOGGER.info("Loading to DB finished.");
     }
 }
