6cd64882fd0e793a3ecc3857ae1d8a36a610d88e
pkrzesinski <Krzesinski.pawel@gmail.com>
2019-05-13 20:39:48 +0200

    XML data loaded successfully.

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/service/CountryXmlToDBLoader.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/service/CountryXmlToDBLoader.java
@@ -3,16 +3,17 @@ package com.infoshareacademy.jjdd6.errorzy.service;
 import com.infoshareacademy.jjdd6.errorzy.dao.CountryDao;
 import com.infoshareacademy.jjdd6.errorzy.model.CountryModel;
 import com.infoshareacademy.jjdd6.errorzy.xmlunmarshaller.CountrySearch;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.transaction.Transactional;
-import java.util.logging.Logger;
 
 @Singleton
 @Transactional
 public class CountryXmlToDBLoader {
-    private static final Logger LOGGER = Logger.getLogger(CountryXmlToDBLoader.class.getName());
+    private static final Logger LOG = LogManager.getLogger(CountrySearch.class);
 
     @EJB
     private CountryDao countryDao;
@@ -34,11 +35,11 @@ public class CountryXmlToDBLoader {
                     country.getCountryName());
 
             countryDao.save(countryModel);
-            LOGGER.info(countryModel.getCountryName() + ": Added to DB.");
+            LOG.info(countryModel.getCountryName() + ": Added to DB.");
 
             cityXmlToDBLoader.loadCityModelToDataBase(country, countryModel);
         });
 
-        LOGGER.info("Loading to DB finished.");
+        LOG.info("Loading to DB finished.");
     }
 }
