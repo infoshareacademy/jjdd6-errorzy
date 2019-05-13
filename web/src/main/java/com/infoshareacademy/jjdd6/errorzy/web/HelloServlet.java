1603264884aace9a987b8f1ffc91a2d63ad301fa
piokowalski <piokowalski@onet.pl>
2019-05-13 14:53:15 +0200

    Loggers configuration working! (Logging into file)

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/HelloServlet.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/HelloServlet.java
@@ -2,6 +2,8 @@ package com.infoshareacademy.jjdd6.errorzy.web;
 
 import com.infoshareacademy.jjdd6.errorzy.freemarker.TemplateProvider;
 import freemarker.template.Template;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -14,13 +16,17 @@ import java.io.IOException;
 @WebServlet("/")
 public class HelloServlet extends HttpServlet {
 
+    private static final Logger LOG = LogManager.getLogger(HelloServlet.class);
+
     @Inject
     private TemplateProvider templateProvider;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOG.info(" HELLO XD ");
         resp.setContentType("text/html;charset=UTF-8");
         Template template = templateProvider.getTemplate(getServletContext(), "hello.ftlh");
         template.dump(resp.getWriter());
+
     }
 }
