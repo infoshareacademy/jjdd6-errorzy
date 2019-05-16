9226957df1f7d938d76731436d605d0913188a44
piokowalski <piokowalski@onet.pl>
2019-05-16 16:41:37 +0200

    Initial commit of contact form feature - ContactFormServlet created, and contact.html prepared to use with all application

---
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/ContactFormServlet.java
@@ -0,0 +1,28 @@
+package com.infoshareacademy.jjdd6.errorzy.web;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/contact")
+public class ContactFormServlet extends HttpServlet {
+
+    private static final Logger LOG = LogManager.getLogger(ContactFormServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
