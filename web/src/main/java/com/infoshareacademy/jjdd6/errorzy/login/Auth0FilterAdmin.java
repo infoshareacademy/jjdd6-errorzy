ae1a17082881482149f5efed1119a4a070111b05
Piotr Kowalski <piokowalski@onet.pl>
2019-05-20 16:20:18 +0200

    Jjdd6 err 9 (#61)
    * New logging points in new classes
    
    * Update logs points in new classes from the freshest develop branch.
    
    * Update logs points in new classes from the freshest develop branch.
    
    * Update logs points in new classes from the freshest develop branch - checked conflicts

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/login/Auth0FilterAdmin.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/login/Auth0FilterAdmin.java
@@ -1,6 +1,8 @@
 package com.infoshareacademy.jjdd6.errorzy.login;
 
 import com.auth0.SessionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -10,11 +12,15 @@ import java.io.IOException;
 
 @WebFilter(urlPatterns = {"/administration", "/loadToDB"})
 public class Auth0FilterAdmin implements Filter {
+
+    private static final Logger LOG = LogManager.getLogger(Auth0FilterAdmin.class);
+
     Boolean isUserAdmin;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Boolean isUserAdmin = Boolean.valueOf(filterConfig.getInitParameter("IsUserAdmin"));
+        LOG.info("Checking authorisation of user");
     }
 
     /**
@@ -24,14 +30,17 @@ public class Auth0FilterAdmin implements Filter {
      * @param response the response to send
      * @param next     the next filter chain
      **/
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain next) throws IOException, ServletException {
+        LOG.info("Filter method in authorisation of admins class has been runned");
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String accessToken = (String) SessionUtils.get(req, "accessToken");
         String idToken = (String) SessionUtils.get(req, "idToken");
         if (accessToken == null && idToken == null) {
             res.sendRedirect("/login");
+
             return;
         }
         next.doFilter(request, response);
@@ -39,6 +48,7 @@ public class Auth0FilterAdmin implements Filter {
 
     @Override
     public void destroy() {
+        LOG.info("Authentication failed.");
         isUserAdmin = false;
     }
 }
--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/login/Auth0FilterUser.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/login/Auth0FilterUser.java
@@ -1,6 +1,8 @@
 package com.infoshareacademy.jjdd6.errorzy.login;
 
 import com.auth0.SessionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -11,8 +13,9 @@ import java.io.IOException;
 /**
  * Filter class to check if a valid session exists. This will be true if the User Id is present.
  */
-@WebFilter(urlPatterns = {"portal/home", "/bike-servlet/*", "/show-city-list", "/closestPlace-servlet", "/country-servlet", "/number-of-places", "/upload", "/country-statistics", "/city-statistics", "/place-statistics", "/mail", "/upload" } )
+@WebFilter(urlPatterns = {"portal/home", "/bike-servlet/*", "/show-city-list", "/closestPlace-servlet", "/country-servlet", "/number-of-places", "/upload", "/country-statistics", "/city-statistics", "/place-statistics", "/mail", "/upload"})
 public class Auth0FilterUser implements Filter {
+    private static final Logger LOG = LogManager.getLogger(Auth0FilterUser.class);
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -27,6 +30,7 @@ public class Auth0FilterUser implements Filter {
      **/
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain next) throws IOException, ServletException {
+        LOG.info("Filtering users method in authorisation of users class has been runned.");
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String accessToken = (String) SessionUtils.get(req, "accessToken");
