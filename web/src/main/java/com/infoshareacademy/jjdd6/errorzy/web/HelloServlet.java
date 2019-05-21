a7073c89a817088e42fb6580199aa1150202b072
Michal Nowakowski <michal@nowakowski.me.uk>
2019-05-21 10:06:52 +0200

    Revert "Wk maps (#68)"
    This reverts commit 1c8912c0d9ec25c0d572796ed309f9dce941b7bd.

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/HelloServlet.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/web/HelloServlet.java
@@ -52,3 +52,36 @@ public class HelloServlet extends HttpServlet {
         }
     }
 }
+
+// package com.infoshareacademy.jjdd6.errorzy.web;
+
+// import com.infoshareacademy.jjdd6.errorzy.freemarker.TemplateProvider;
+// import freemarker.template.Template;
+// import org.apache.logging.log4j.LogManager;
+// import org.apache.logging.log4j.Logger;
+
+// import javax.inject.Inject;
+// import javax.servlet.ServletException;
+// import javax.servlet.annotation.WebServlet;
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+// import java.io.IOException;
+
+// @WebServlet("/portal/home")
+// public class HelloServlet extends HttpServlet {
+
+//     private static final Logger LOG = LogManager.getLogger(HelloServlet.class);
+
+//     @Inject
+//     private TemplateProvider templateProvider;
+
+//     @Override
+//     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//         LOG.info("Hello Servlet has been opened.");
+//         resp.setContentType("text/html;charset=UTF-8");
+//         Template template = templateProvider.getTemplate(getServletContext(), "hello.ftlh");
+//         template.dump(resp.getWriter());
+
+//     }
+// }
