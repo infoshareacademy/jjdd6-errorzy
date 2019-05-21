d0dbeca4bad06ba9ad78672538d8388e9aabc2bd
Michal Nowakowski <michal@nowakowski.me.uk>
2019-05-21 09:41:27 +0200

    Revert "Admin Credentials (#67)"
    This reverts commit 13faf93436f60ad8f21b3ac2477696c06f02b891.

--- web/src/main/java/com/infoshareacademy/jjdd6/errorzy/login/Auth0FilterAdmin.java
+++ web/src/main/java/com/infoshareacademy/jjdd6/errorzy/login/Auth0FilterAdmin.java
@@ -8,17 +8,19 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter(urlPatterns = {"/admin", "/db-load", "/statistics", "/mail"})
+@WebFilter(urlPatterns = {"/administration", "/loadToDB"})
 public class Auth0FilterAdmin implements Filter {
 
     private static final Logger LOG = LogManager.getLogger(Auth0FilterAdmin.class);
 
+    Boolean isUserAdmin;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Boolean isUserAdmin = Boolean.valueOf(filterConfig.getInitParameter("IsUserAdmin"));
+        LOG.info("Checking authorisation of user");
     }
 
     /**
@@ -28,33 +30,26 @@ public class Auth0FilterAdmin implements Filter {
      * @param response the response to send
      * @param next     the next filter chain
      **/
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain next) throws IOException, ServletException {
         LOG.info("Filter method in authorisation of admins class has been runned");
-
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
-
         String accessToken = (String) SessionUtils.get(req, "accessToken");
         String idToken = (String) SessionUtils.get(req, "idToken");
         if (accessToken == null && idToken == null) {
             res.sendRedirect("/login");
+
             return;
         }
-
-
-        HttpSession session = req.getSession();
-        Object isAdminObj = session.getAttribute("isAdmin");
-
-        if (isAdminObj != null && ((Boolean) isAdminObj).booleanValue()) {
-            next.doFilter(request, response);
-        }
-
-        res.setStatus(401);
+        next.doFilter(request, response);
     }
 
     @Override
     public void destroy() {
+        LOG.info("Authentication failed.");
+        isUserAdmin = false;
     }
 }
 
